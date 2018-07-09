29394b2464041d6743f7457c24972d8e69d5a2b0
NamNT <thanhnamk96@gmail.com>
2018-07-09 22:43:47 +0700

    fix issue again

--- App.js
+++ App.js
@@ -28,9 +28,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text>Nguyen Thanh Nam
-         {console.log("Day là log")}
-        </Text>
+        <Text>Nguyen Thanh Nam</Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
